51bcd522b13e3f095ca4ed236c3d576ce9fecf86
SODA Noriyuki <n-soda@users.sourceforge.net>
2024-01-12 10:19:57 +0900

    tls_test: add tls_proxy_certificate to struct tls_test_ctx_struct

--- regress/manual/lib/libgfarm/gfarm/tls-test/tls-test.c
+++ regress/manual/lib/libgfarm/gfarm/tls-test/tls-test.c
@@ -21,7 +21,6 @@
 static int debug_level = 0;
 static int buf_size = 65536;
 static bool is_server = false;
-static bool is_proxy = false;
 static bool is_mutual_authentication = false;
 static bool is_verify_only = false;
 static bool is_once = false;
@@ -40,6 +39,7 @@ static struct tls_test_ctx_struct ttcs = {
 	-INT_MAX,
 	0,	/* No domain check. Mandatory to initialize properly 0 or 1 */
 	0,	/* No domain check. Mandatory to initialize properly 0 or 1 */
+	0,	/* do not allow proxy certificate */
 	0,	/* security level 0, because tls_test generates 1024bit cert */
 	60,
 	60
@@ -147,6 +147,8 @@ ctx_dump()
 			gfarm_ctxp->tls_build_chain_local);
 	fprintf(stderr, "tls_allow_no_crl: %d\n",
 			gfarm_ctxp->tls_allow_no_crl);
+	fprintf(stderr, "tls_proxy_certificate: %d\n",
+			gfarm_ctxp->tls_proxy_certificate);
 	fprintf(stderr, "network_receive_timeout: %d\n",
 			gfarm_ctxp->network_receive_timeout);
 	fprintf(stderr, "network_send_timeout: %d\n",
@@ -159,7 +161,7 @@ static inline void
 getopt_arg_dump()
 {
 	fprintf(stderr, "is_server: %d\n", is_server);
-	fprintf(stderr, "is_proxy: %d\n", is_proxy);
+	fprintf(stderr, "is_proxy: %d\n", gfarm_ctxp->tls_proxy_certificate);
 	fprintf(stderr, "address: '%s'\n", ipaddr);
 	fprintf(stderr, "portnum: '%s'\n", portnum);
 	fprintf(stderr, "tls_cipher_suite: '%s'\n",
@@ -318,7 +320,7 @@ prologue(int argc, char **argv, struct addrinfo **a_info)
 			gfarm_ctxp->tls_allow_no_crl = 1;
 			break;
 		case 17:
-			is_proxy = true;
+			gfarm_ctxp->tls_proxy_certificate = 1;
 			break;
 		case 's':
 			is_server = true;
@@ -820,7 +822,7 @@ main(int argc, char **argv)
 				(is_server == true) ?
 				TLS_ROLE_SERVER : TLS_ROLE_CLIENT,
 				is_mutual_authentication,
-				is_proxy);
+				gfarm_ctxp->tls_proxy_certificate);
 		if (gerr == GFARM_ERR_NO_ERROR) {
 			ret = (is_server == true) ?
 				run_server(tls_ctx, a_info)
